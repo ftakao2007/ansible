c59ce0e0d26ffb5fa2694564af3623205f84bf1a
Takao Fukui <ftakao2007@gmail.com>
2015-03-07 02:07:14 +0900

    add mysql centos6

--- init_user.sh
+++ init_user.sh
@@ -21,9 +21,9 @@ read ssh_private_key_path
 if [ -z "$ssh_private_key_path" ]; then ssh_private_key_path="~/.ssh/id_rsa"; fi
 
 ### for inventory setting
-echo "hostname? [default : node]"
-read host_name
-if [ -z "$host_name" ]; then host_name="node"; fi
+#echo "hostname? [default : node]"
+#read host_name
+#if [ -z "$host_name" ]; then host_name="node"; fi
 
 echo "group? [default : webservers]"
 read group
@@ -31,7 +31,9 @@ if [ -z "$group" ]; then group=webservers; fi
 
 ### create inventory
 echo "[${group}]" > ${inventoryfile}
-echo "${host_name}" >> ${inventoryfile}
+echo "${ssh_ip}" >> ${inventoryfile}
+# for host_vars
+#echo "${host_name}" >> ${inventoryfile}
 
 ### create group_vars
 echo "ansible_ssh_user: \"${ssh_user}\"" > group_vars/${group}
@@ -39,4 +41,4 @@ echo "ansible_ssh_port: \"${ssh_port}\"" >> group_vars/${group}
 echo "ansible_ssh_private_key_file: \"${ssh_private_key_path}\"" >> group_vars/${group}
 
 ### create host_vars
-echo "ansible_ssh_host: \"${ssh_ip}\"" > host_vars/${host_name}
+#echo "ansible_ssh_host: \"${ssh_ip}\"" > host_vars/${host_name}
